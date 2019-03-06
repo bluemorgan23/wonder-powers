a5059e62d3a7b7afd585319536ab008b5b5c484b
Chris Morgan <bluemorgan2323@gmail.com>
2019-03-06 14:15:30 -0600

    Created a function that could be plugged into each of the eventListener functions for each button.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,30 +8,29 @@ const xraySection = document.querySelector("#xray");
 
 const allSections = document.querySelectorAll(".power");
 
+//Code for original exercise :
 
+// document.querySelector("#activate-flight").addEventListener("click", function () {
+//     flightSection.classList.remove("disabled");
+//     flightSection.classList.add("enabled");
+// })
 
+// document.querySelector("#activate-mindreading").addEventListener("click", function () {
+//     mindreadingSection.classList.remove("disabled");
+//     mindreadingSection.classList.add("enabled");
+// })
 
-document.querySelector("#activate-flight").addEventListener("click", function () {
-    flightSection.classList.remove("disabled");
-    flightSection.classList.add("enabled");
-})
+// document.querySelector("#activate-xray").addEventListener("click", function () {
+//     xraySection.classList.remove("disabled");
+//     xraySection.classList.add("enabled");
+// })
 
-document.querySelector("#activate-mindreading").addEventListener("click", function () {
-    mindreadingSection.classList.remove("disabled");
-    mindreadingSection.classList.add("enabled");
-})
-
-document.querySelector("#activate-xray").addEventListener("click", function () {
-    xraySection.classList.remove("disabled");
-    xraySection.classList.add("enabled");
-})
-
-document.querySelector("#activate-all").addEventListener("click", function () {
-    for (let i = 0; i < allSections.length; i++) {
-        allSections[i].classList.remove("disabled");
-        allSections[i].classList.add("enabled");
-    }
-})
+// document.querySelector("#activate-all").addEventListener("click", function () {
+//     for (let i = 0; i < allSections.length; i++) {
+//         allSections[i].classList.remove("disabled");
+//         allSections[i].classList.add("enabled");
+//     }
+// })
 
 document.querySelector("#deactivate-all").addEventListener("click", function () {
     for (let i = 0; i < allSections.length; i++) {
@@ -39,3 +38,29 @@ document.querySelector("#deactivate-all").addEventListener("click", function () 
         allSections[i].classList.add("disabled");
     }
 })
+
+// Code below is for the challenge exercise One Function to Rule Them All
+
+const oneFunctionRules = (section) => {
+    section.classList.remove("disbled");
+    section.classList.add("enabled");
+}
+
+document.querySelector("#activate-flight").addEventListener("click", function () {
+    oneFunctionRules(flightSection);
+})
+
+document.querySelector("#activate-mindreading").addEventListener("click", function () {
+    oneFunctionRules(mindreadingSection);
+})
+
+document.querySelector("#activate-xray").addEventListener("click", function () {
+    oneFunctionRules(xraySection);
+})
+
+document.querySelector("#activate-all").addEventListener("click", function () {
+    for (let i = 0; i < allSections.length; i++) {
+        oneFunctionRules(allSections[i]);
+    }
+})
+
